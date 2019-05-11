3350b6b321fbf059c5e9c587d5ad5bf77a488aad
Felipe Leonardo Furtado Barros <felipelfbarros@gmail.com>
2019-05-11 16:17:19 -0300

    Create models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Client(models.Model):
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    name = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    telephone = models.CharField(max_length=15)
+    cpf = models.CharField(max_length=14)
+
+
+class Loan(models.Model):
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    client = models.ForeignKey(Client, on_delete=models.PROTECT)
+    amount = models.DecimalField(decimal_places=2, max_digits=10)
+    term = models.IntegerField()
+    rate = models.FloatField()
+    date = models.DateField(auto_now_add=True)
+    installment = models.DecimalField(decimal_places=2, max_digits=10, blank=True, null=True)
+
+
+class Payment(models.Model):
+    loan = models.ForeignKey(Loan, on_delete=models.PROTECT)
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    payment = models.CharField(max_length=6)
+    date = models.DateField(auto_now_add=True)
+    amount = models.DecimalField(decimal_places=2, max_digits=10)

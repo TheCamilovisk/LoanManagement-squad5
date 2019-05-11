74db96dc025b46b70511799ec6ca40c06ddc4633
Felipe Leonardo Furtado Barros <felipelfbarros@gmail.com>
2019-05-11 18:02:11 -0300

    Register models on admin page

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from core.models import Client, Loan, Payment
 
-# Register your models here.
+admin.site.register(Client)
+admin.site.register(Loan)
+admin.site.register(Payment)

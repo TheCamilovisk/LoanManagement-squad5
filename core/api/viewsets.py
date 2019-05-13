3eac34805dbd733bec551e37ecf76a7c3bc3cdc1
Felipe Barros <felipelfbarros@gmail.com>
2019-05-13 17:25:51 -0300

    Add create for ClientViewSet

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -2,11 +2,23 @@ from rest_framework.viewsets import ModelViewSet
 from core.api.serializers import ClientSerializer, LoanSerializer, PaymentSerializer
 from core.models import Client, Loan, Payment
 from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth.models import User
+from core.utils import calc_installment
 
 
 class ClientViewSet(ModelViewSet):
     queryset = Client.objects.all()
     serializer_class = ClientSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = ClientSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            headers = self.get_success_headers(serializer.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class LoanViewSet(ModelViewSet):

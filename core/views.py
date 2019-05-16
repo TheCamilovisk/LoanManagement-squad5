b63b18610f4081af319eec8ad30b587e15acc0d7
Camilo <camilolgon@gmail.com>
2019-05-16 18:39:40 -0300

    Adding POST handling.

--- core/views.py
+++ core/views.py
@@ -1,28 +1,28 @@
-from django.shortcuts import render
 from rest_framework import status
-from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from core.api.serializers import LoanSerializer, ClientSerializer, PaymentSerializer, LoanCreateSerializer
+from rest_framework.response import Response
+
+from core.api.serializers import (
+    ClientSerializer,
+    LoanCreateSerializer,
+    LoanSerializer,
+    PaymentSerializer,
+)
 from core.models import Client, Loan, Payment
+
 
 @api_view(['GET', 'POST'])
 def loans(request, format=None):
     if request.method == 'POST':
         serializer = LoanCreateSerializer(data=request.data)
-        #4
         if serializer.is_valid():
             amount = float(request.data['amount'])
             term = int(request.data['term'])
             rate = float(request.data['rate'])
-            r = rate/term
+            r = rate / term
             installment = (r + r / ((1 + r) ** term - 1)) * amount
             serializer.save(user=request.user, installment=installment)
-            #headers = self.get_sucess_headers(serializer.data)
-            return Response(
-                serializer.data,
-                status=status.HTTP_201_CREATED,
-            #    headers=headers
-            )
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     elif request.method == 'GET':
         loans = Loan.objects.all()
@@ -37,7 +37,10 @@ def clients(request, format=None):
         serializer = ClientSerializer(clients, many=True)
         return Response(serializer.data)
     elif request.method == 'POST':
-        pass #TODO
+        serializer = ClientSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(user=request.user)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 @api_view(['GET', 'POST'])
@@ -47,10 +50,10 @@ def payments(request, pk, format=None):
         serializer = PaymentSerializer(payments, many=True)
         return Response(serializer.data)
     elif request.method == 'POST':
-        pass #TODO
+        pass  # TODO
 
 
 @api_view(['POST'])
 def balance(request, pk, format=None):
     if request.method == 'POST':
-        pass #TODO
+        pass  # TODO

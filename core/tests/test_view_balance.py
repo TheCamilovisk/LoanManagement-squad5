5d7bfd988be1e58f1ac2186ca5d9e8f8f85819f7
fnscoder <fnscoder@gmail.com>
2019-05-16 22:43:39 -0300

    Create view balance tests

--- core/tests/test_view_balance.py
+++ core/tests/test_view_balance.py
@@ -84,7 +84,45 @@ class BalanceTest(TestCase):
             )
             p.save()
 
-        self.response_invalid = self.client.post('/loans/0/balance/')
-        self.response_valid = self.client.post('/loans/1/balance/')
+        self.response_loan_not_found = self.client.post('/loans/0/balance/')
+        self.response_with_payments = self.client.post('/loans/1/balance/')
         self.response_no_payments = self.client.post('/loans/2/balance/')
         self.response_loan_paid = self.client.post('/loans/3/balance/')
+        self.response_invalid_id = self.client.post('/loans/a/balance/')
+
+    def test_url_resolves_balance_view(self):
+        """URL /loans/1/balance/ must use view balance"""
+        view = resolve('/loans/1/balance/')
+        self.assertEquals(view.func, views.balance)
+
+    def test_balance_post_status_code(self):
+        """POST /loans/1/balance/ must return status code 200"""
+        self.assertEqual(200, self.response_with_payments.status_code)
+
+    def test_balance_value(self):
+        """POST /loans/1/balance/ must contains 856.00"""
+        self.assertContains(self.response_with_payments, 856.00)
+
+    def test_balance_loan_not_found_status_code(self):
+        """POST /loans/0/balance/ must return status code 200"""
+        self.assertEqual(200, self.response_loan_not_found.status_code)
+
+    def test_balance_loan_not_found_content(self):
+        """POST /loans/0/balance/ must contains Loan not found"""
+        self.assertContains(self.response_loan_not_found, 'Loan not found')
+
+    def test_balance_loan_with_no_payments_value(self):
+        """POST /loans/2/balance/ must contains 1027.20"""
+        self.assertContains(self.response_no_payments, 1027.20)
+
+    def test_balance_loan_paid_status_code(self):
+        """POST /loans/3/balance/ must return status code 200"""
+        self.assertEqual(200, self.response_loan_paid.status_code)
+
+    def test_balance_loan_paid_value(self):
+        """POST /loans/3/balance/ must contains 0.00"""
+        self.assertContains(self.response_loan_paid, 0.00)
+
+    def test_invalid_id(self):
+        """POST /loans/a/balance must return status code 404"""
+        self.assertEqual(404, self.response_invalid_id.status_code)

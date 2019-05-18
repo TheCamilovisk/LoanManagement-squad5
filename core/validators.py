c647adb91b75a6f1e23a32633d7b9d7ff7420de8
Camilo Gonçalves <camilolgon@gmail.com>
2019-05-18 17:01:17 -0300

    Adding the cpf validator.

--- core/validators.py
+++ core/validators.py
@@ -1,14 +1,17 @@
 from django.core.exceptions import ValidationError
+import re
+
 
 def validate_term(term):
     if term <= 0:
-        raise ValidationError(
-            'Number of terms has to be a positive integer number.'
-            )
+        raise ValidationError('Number of terms has to be a positive integer number.')
 
 
 def validate_amount(amount):
     if amount <= 0:
-        raise ValidationError(
-            'Amount has to be a positive real number.'
-        )
+        raise ValidationError('Amount has to be a positive real number.')
+
+
+def validate_cpf(cpf):
+    if re.compile(r'^[\d]{11}$').match(cpf) is None or cpf == '00000000000':
+        raise ValidationError('The given CPF is invalid.')

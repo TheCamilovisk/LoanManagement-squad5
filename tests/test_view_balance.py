62c2b52918d3949554c472430bf816dfcfbf21ef
fnscoder <fnscoder@gmail.com>
2019-05-20 15:20:12 -0300

    Refactor for pep8

--- tests/test_view_balance.py
+++ tests/test_view_balance.py
@@ -11,41 +11,38 @@ from core.models import Loan, Payment, Client
 
 class BalanceTest(APITestCase):
     def setUp(self):
-        self.user = User.objects.create_user(username='john', email='john@doe.com', password='topsecretpwd')
-        self.client.login(username='john', password='topsecretpwd')
+        self.user = User.objects.create_user(
+            username="john", email="john@doe.com", password="topsecretpwd"
+        )
+        self.client.login(username="john", password="topsecretpwd")
 
         client_data = {
-            'name': 'Felicity',
-            'surname': 'Jones',
-            'email': 'felicity@gmail.com',
-            'telephone': '11984345678',
-            'cpf': '34598712376',
+            "name": "Felicity",
+            "surname": "Jones",
+            "email": "felicity@gmail.com",
+            "telephone": "11984345678",
+            "cpf": "34598712376",
         }
-        self.client.post('/clients/', client_data, format='json')
+        self.client.post("/clients/", client_data, format="json")
 
-        new_loan = {
-            'client': 1,
-            'amount': 1000,
-            'term': 12,
-            'rate': 0.05
-        }
-        self.client.post('/loans/', new_loan, format='json')
+        new_loan = {"client": 1, "amount": 1000, "term": 12, "rate": 0.05}
+        self.client.post("/loans/", new_loan, format="json")
         self.assertEqual(Loan.objects.all().count(), 1)
 
         payment1 = Payment(
             loan=Loan.objects.get(pk=1),
             user=self.user,
-            payment='made',
-            date=datetime.strptime('10042019', '%d%m%Y'),
+            payment="made",
+            date=datetime.strptime("10042019", "%d%m%Y"),
             amount=85.6,
         )
         payment1.save()
-        
+
         payment2 = Payment(
             loan=Loan.objects.get(pk=1),
             user=self.user,
-            payment='made',
-            date=datetime.strptime('10052019', '%d%m%Y'),
+            payment="made",
+            date=datetime.strptime("10052019", "%d%m%Y"),
             amount=85.6,
         )
         payment2.save()
@@ -56,17 +53,17 @@ class BalanceTest(APITestCase):
             amount=1000,
             term=12,
             rate=0.5,
-            date=datetime.strptime('10122017', '%d%m%Y'),
-            installment=85.6
+            date=datetime.strptime("10122017", "%d%m%Y"),
+            installment=85.6,
         )
         loan_paid.save()
-        
+
         for month in range(1, 13):
             p = Payment(
                 loan=Loan.objects.get(pk=2),
                 user=self.user,
-                payment='made',
-                date=datetime.strptime(f'10{month}2018', '%d%m%Y'),
+                payment="made",
+                date=datetime.strptime(f"10{month}2018", "%d%m%Y"),
                 amount=85.6,
             )
             p.save()
@@ -77,26 +74,24 @@ class BalanceTest(APITestCase):
             amount=1000,
             term=12,
             rate=0.5,
-            date=datetime.strptime('10032019', '%d%m%Y'),
-            installment=85.6
+            date=datetime.strptime("10032019", "%d%m%Y"),
+            installment=85.6,
         )
         loan_with_no_payments.save()
 
-        self.response_with_payments = self.client.get('/loans/1/balance/')
-        self.response_loan_paid = self.client.get('/loans/2/balance/')
-        self.response_no_payments = self.client.get('/loans/3/balance/')
-        self.response_loan_not_found = self.client.get('/loans/0/balance/')
-        self.response_invalid_id = self.client.get('/loans/a/balance/')
-
-
+        self.response_with_payments = self.client.get("/loans/1/balance/")
+        self.response_loan_paid = self.client.get("/loans/2/balance/")
+        self.response_no_payments = self.client.get("/loans/3/balance/")
+        self.response_loan_not_found = self.client.get("/loans/0/balance/")
+        self.response_invalid_id = self.client.get("/loans/a/balance/")
 
     def test_url_resolves_balance_view(self):
         """URL /loans/1/balance/ must use view balance"""
-        view = resolve('/loans/1/balance/')
+        view = resolve("/loans/1/balance/")
         self.assertEquals(view.func, views.balance)
 
     def test_payment(self):
-        response = self.client.get('/loans/1/payments/')
+        response = self.client.get("/loans/1/payments/")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_balance_loan_with_two_payments(self):
@@ -125,8 +120,10 @@ class BalanceTest(APITestCase):
 
     def test_balance_loan_not_found_content(self):
         """GET /loans/0/balance/ must contains Loan not found"""
-        self.assertContains(self.response_loan_not_found, 'Loan not found')
+        self.assertContains(self.response_loan_not_found, "Loan not found")
 
     def test_invalid_id(self):
         """GET /loans/a/balance must return status code 404"""
-        self.assertEqual(self.response_invalid_id.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertEqual(
+            self.response_invalid_id.status_code, status.HTTP_404_NOT_FOUND
+        )
